76328ced9c6cca5329d35d300c47ce64e986b0a1
Maher Al Abbasi <maalabba@cisco.com>
2020-12-02 19:37:53 +0200

    Added addunare

--- src/com/maher/unproiect/Main.java
+++ src/com/maher/unproiect/Main.java
@@ -2,7 +2,10 @@ package com.maher.unproiect;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        int x = 2;
+        int y = 3;
+        System.out.println(2 + 3);
     }
 }
